ce052001765c72227046694c4d591470aabbd3ae
Jacques <jacques@jawisoftbcd.com>
2023-11-16 15:20:58 +0200

    Added Cookie Concent Again.
    Testing if it works.

--- js/cookie.js
+++ js/cookie.js
@@ -1,25 +1,48 @@
+const cookieConsentBanner = document.querySelector('#cookie-consent-banner');
+const acceptButton = document.querySelector('#cookie-consent-banner__accept-button');
+const rejectButton = document.querySelector('#cookie-consent-banner__reject-button');
+
+document.cookie = "username=John Doe; expires=Thu, 18 Dec 2013 12:00:00 UTC; path=/";
 function getCookie(name) {
-    const value = "; " + document.cookie;
-    const parts = value.split("; " + name + "=");
-    if (parts.length === 2) {
-        return parts.pop().split(";").shift();
+    // Split cookie string and get all individual name=value pairs in an array
+    var cookieArr = document.cookie.split(";");
+    
+    // Loop through the array elements
+    for(var i = 0; i < cookieArr.length; i++) {
+        var cookiePair = cookieArr[i].split("=");
+        /* Removing whitespace at the beginning of the cookie name
+        and compare it with the given string */
+        if(name == cookiePair[0].trim()) {
+            // Decode the cookie value and return
+            return decodeURIComponent(cookiePair[1]);
+        }
     }
+    
+    // Return null if not found
+    return null;
 }
 
-function setCookie(name, value, days) {
-    const date = new Date();
-    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-    const expires = "expires=" + date.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-}
-
-function acceptCookies() {
-    setCookie("acceptedCookies", "true", 30);
-    document.getElementById("cookiePopup").style.display = "none";
-}
-
-window.addEventListener("DOMContentLoaded", function () {
-    if (!getCookie("acceptedCookies")) {
-        document.getElementById("cookiePopup").style.display = "block";
+const checkCookie = () => {
+    cookieConsentBanner.style.display = 'none';
+    var concet_obtained = getCookie("cookieConsent");
+    if (concet_obtained == 'accept') {
+        var ga = document.createElement('script');
+        ga.src = 'https://www.googletagmanager.com/gtag/js?id=YOUR_GA_MEASUREMENT_ID';
+        ga.async = true;
+        document.head.appendChild(ga);
+        return;
     }
+    cookieConsentBanner.style.display = 'block';
+};
+
+checkCookie();
+
+acceptButton.addEventListener('click', () => {
+    document.cookie = 'cookieConsent=accept; max-age=2592000; path=/';
+    window.location.reload();
 });
+
+rejectButton.addEventListener('click', () => {
+    cookieConsentBanner.style.display = 'none';
+});
+
